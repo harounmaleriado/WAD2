468bdaa610ab917641cdd607983a859dda9dc4a5
JonIHolton <142396435+JonIHolton@users.noreply.github.com>
2023-10-29 15:54:41 +0800

    Creating UserDTO

--- SpringBoot/src/main/java/ewasteless/project/controllers/UserAuthenticationController.java
+++ SpringBoot/src/main/java/ewasteless/project/controllers/UserAuthenticationController.java
@@ -1,12 +1,16 @@
 package ewasteless.project.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import ewasteless.project.DTO.UserDTO;
 import ewasteless.project.service.UserAuthenticationService;
 
 @RestController
@@ -16,19 +20,14 @@ public class UserAuthenticationController {
     @Autowired
     private UserAuthenticationService authService;
 
+    
     @PostMapping("/signup")
-    public ResponseEntity<String> signUp(
-        @RequestParam String email, 
-        @RequestParam String password, 
-        @RequestParam String name, 
-        @RequestParam String username) {
-        return ResponseEntity.ok(authService.signUp(email, password, name, username));
+    public ResponseEntity<String> signUp(@Valid @RequestBody UserDTO user) {
+        return ResponseEntity.ok(authService.signUp(user));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> signIn(
-        @RequestParam String email, 
-        @RequestParam String password) {
-        return ResponseEntity.ok(authService.signIn(email, password));
+    public ResponseEntity<String> signIn(@RequestBody UserDTO user) {
+        return ResponseEntity.ok(authService.signIn(user.getEmail(), user.getPassword()));
     }
 }
