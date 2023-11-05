d7342ce4ae300dae79b1aca5f78ce5b7748834f6
JonIHolton <142396435+JonIHolton@users.noreply.github.com>
2023-11-06 02:43:58 +0800

    fixed

--- SpringBoot/src/main/java/ewasteless/project/FirebaseConfig.java
+++ SpringBoot/src/main/java/ewasteless/project/FirebaseConfig.java
@@ -29,10 +29,10 @@ public class FirebaseConfig {
             // Firebase SDK credential file
             FileInputStream serviceAccount = 
             // correct one
-            // new FileInputStream("SpringBoot/src/main/resources/FirebaseKey.json");
+            new FileInputStream("SpringBoot/src/main/resources/FirebaseKey.json");
 
             // test
-            new FileInputStream("SpringBoot/src/main/resources/is216-proj-76064-firebase-adminsdk-7j8ba-871e4404cb.json");
+            // new FileInputStream("SpringBoot/src/main/resources/is216-proj-76064-firebase-adminsdk-7j8ba-871e4404cb.json");
             FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 // .setDatabaseUrl("https://is216-e-wasteless-default-rtdb.asia-southeast1.firebasedatabase.app")
