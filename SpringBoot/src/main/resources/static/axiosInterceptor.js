d16b2ea5bcc5adf0da0ad65ad9d6db42a81b3e8d
JonIHolton <142396435+JonIHolton@users.noreply.github.com>
2023-11-06 14:28:08 +0800

    Cleaned up and renamed axios interceptor

--- SpringBoot/src/main/resources/static/axiosInterceptor.js
+++ SpringBoot/src/main/resources/static/axiosInterceptor.js
@@ -1,12 +1,12 @@
-// Set up Axios interceptor
+// Axios interceptor
 axios.interceptors.request.use(async function(config) {
     
-    
-    console.log("Interceptor called, Token:", token);
+    const token = sessionStorage.getItem('idToken');
+    console.log("Interceptor called, Token:");
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
+        console.log("Token added to Authorisation header");
     }
-    console.log("Token added to Authorisation header")
     return config;
 }, function(error) {
     return Promise.reject(error);
